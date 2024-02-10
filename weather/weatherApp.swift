df8ec3f225edc5679df71b5b41fc140eb8361f3c
Arsham1024 <Arsham512@gmail.com>
2024-02-09 20:48:15 -0800

    Initial Commit

---
+++ weather/weatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  weatherApp.swift
+//  weather
+//
+//  Created by Arsham Mehrani on 2/9/24.
+//
+
+import SwiftUI
+
+@main
+struct weatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
